14a0cb147d7a72ab25a0d9dc1fbc632363534256
zy <xtagwgj@163.com>
2017-06-14 09:35:53 +0800

    添加滚动数字

--- Demo/app/src/main/java/com/xtagwgj/baseprojectdemo/MainActivity.java
+++ Demo/app/src/main/java/com/xtagwgj/baseprojectdemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.xtagwgj.baseprojectdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.xtagwgj.baseproject.view.NumberRunningTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberRunningTextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        viewById = (NumberRunningTextView) findViewById(R.id.numberRunning);
+        viewById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
     }
 }
